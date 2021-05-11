ead4b4cfb00fb2a493e27439ccd0f5b5c16a1ae9
alan10fm <alan10fm@gmail.com>
2021-05-11 17:22:01 -0500

    Adding platform specific key generation

--- src/main/java/com/netki/keymanagement/service/impl/KeyManagementNetkiService.kt
+++ src/main/java/com/netki/keymanagement/service/impl/KeyManagementNetkiService.kt
@@ -1,6 +1,7 @@
 package com.netki.keymanagement.service.impl
 
 import com.netki.exceptions.*
+import com.netki.exceptions.ExceptionInformation.CERTIFICATE_INFORMATION_STRING_NOT_CORRECT_ERROR_PROVIDER
 import com.netki.exceptions.ExceptionInformation.KEY_MANAGEMENT_CERTIFICATE_INVALID_EXCEPTION
 import com.netki.exceptions.ExceptionInformation.KEY_MANAGEMENT_ERROR_FETCHING_CERTIFICATE
 import com.netki.exceptions.ExceptionInformation.KEY_MANAGEMENT_ERROR_FETCHING_CERTIFICATE_NOT_FOUND
@@ -9,13 +10,14 @@ import com.netki.exceptions.ExceptionInformation.KEY_MANAGEMENT_ERROR_FETCHING_P
 import com.netki.exceptions.ExceptionInformation.KEY_MANAGEMENT_ERROR_STORING_CERTIFICATE
 import com.netki.exceptions.ExceptionInformation.KEY_MANAGEMENT_ERROR_STORING_PRIVATE_KEY
 import com.netki.exceptions.ExceptionInformation.KEY_MANAGEMENT_PRIVATE_KEY_INVALID_EXCEPTION
+import com.netki.extensions.isAlphaNumeric
 import com.netki.keygeneration.main.KeyGeneration
+import com.netki.keygeneration.util.toPrincipal
 import com.netki.keymanagement.driver.KeyManagementDriver
 import com.netki.keymanagement.service.KeyManagementService
+import com.netki.model.AttestationCertificate
 import com.netki.model.AttestationInformation
-import com.netki.security.toCertificate
-import com.netki.security.toPemFormat
-import com.netki.security.toPrivateKey
+import com.netki.security.*
 import java.security.PrivateKey
 import java.security.cert.X509Certificate
 import java.util.*
@@ -28,8 +30,35 @@ internal class KeyManagementNetkiService(
     /**
      * {@inheritDoc}
      */
-    override fun generateCertificates(attestationsInformation: List<AttestationInformation>) =
-        keyGeneration.generateCertificates(attestationsInformation)
+    override fun generateCertificates(attestationsInformation: List<AttestationInformation>): List<AttestationCertificate> {
+        validateAttestationData(attestationsInformation)
+
+        val keyPair = Keys.generateKeyPair()
+
+        val attestationsCertificate = attestationsInformation.map {
+            AttestationCertificate(
+                attestation = it.attestation,
+                csr = Certificate.csrObjectToPem(
+                    Certificate.generateCSR(it.attestation.toPrincipal(it.data, it.ivmsConstraint), keyPair)
+                ),
+                publicKeyPem = keyPair.public.toPemFormat()
+            )
+        }
+
+        val certificates = keyGeneration.generateCertificates(attestationsCertificate)
+
+        return if (certificates.count() == 0) {
+            emptyList()
+        } else {
+            certificates.map {
+                AttestationCertificate(
+                    it.attestation,
+                    it.certificatePem!!,
+                    keyPair.private.toPemFormat()
+                )
+            }
+        }
+    }
 
     /**
      * {@inheritDoc}
@@ -175,6 +204,19 @@ internal class KeyManagementNetkiService(
         } ?: throw ObjectNotFoundException(KEY_MANAGEMENT_ERROR_FETCHING_PRIVATE_KEY_NOT_FOUND.format(privateKeyId))
     }
 
-
     private fun generateUniqueId() = UUID.randomUUID().toString()
+
+    private fun validateAttestationData(attestationsInformation: List<AttestationInformation>) {
+        attestationsInformation.forEach { information ->
+            if (!information.data.isAlphaNumeric()) {
+                throw CertificateProviderException(
+                    String.format(
+                        CERTIFICATE_INFORMATION_STRING_NOT_CORRECT_ERROR_PROVIDER,
+                        information.data,
+                        information.attestation
+                    )
+                )
+            }
+        }
+    }
 }
